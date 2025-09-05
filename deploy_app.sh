82fb97b68cdf3d861d5ff7c9bdcdb6d3de9de949
Ilia Sazonov <ilia.sazonov@oracle.com>
2025-09-05 00:10:22 -0700

    permissions

--- deploy_app.sh
+++ deploy_app.sh
@@ -2,8 +2,8 @@
 
 cd rwdqueryservice
 git pull
-docker build -t rwdqueryservice .
+sudo docker build -t rwdqueryservice .
 
-docker-compose -p app down app
-docker-compose -p app up -d app
+sudo docker-compose -p app down app
+sudo docker-compose -p app up -d app
 
