1345418732f6bd5ed8752af9b3bca71ce6b57355
Ilia Sazonov <ilia_saz@yahoo.com>
2025-09-11 17:43:07 -0700

    env vars and deployment

--- deploy_app.sh
+++ deploy_app.sh
@@ -4,6 +4,6 @@ cd rwdqueryservice
 git pull
 sudo docker build -t rwdqueryservice .
 
+cd ~
 sudo docker-compose -p app down app
-sudo docker-compose -p app up -d app
-
+sudo docker-compose -f ./docker-compose.yml -p app up -d app
