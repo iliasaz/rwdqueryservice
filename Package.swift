008608d8eb8abdd04b375710e152965a5d202d7c
Abhishek Narayan <abnaraya@Abhisheks-MacBook-Pro.local>
2025-09-11 20:21:59 -0700

    swift-tool-version reverted to 6.1

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
-// swift-tools-version:6.0
+// swift-tools-version:6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-// Adjusted tools version to match installed Swift 6.0.3. If you upgrade Xcode/Swift to 6.1+, revert to 6.1.
 
 import PackageDescription
 
